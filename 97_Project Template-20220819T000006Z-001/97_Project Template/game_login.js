9ea4dd081fb79b0a434e2cb9914e5fc8c5683133
theabhibajaj <89036139+theabhibajaj@users.noreply.github.com>
2022-08-27 23:35:56 -0400

    Add files via upload

---
+++ 97_Project Template-20220819T000006Z-001/97_Project Template/game_login.js
@@ -0,0 +1,12 @@
+// Create function addUser()
+function addUser(){
+  // Get value of user by id player1_name_input and player2_name_input
+  player1_name = document.getElementById("player1_name_input").Value;
+  player2_name = document.getElementById("player2_name_input").Value;
+  // Store these values locally
+  localStorage.setItem("player1_name", player1_name);
+  localStorage.setItem("player2_name", player1_name);
+  //Assign "game_page.html" to window.location
+  window.location = "index.html";
+}
+
